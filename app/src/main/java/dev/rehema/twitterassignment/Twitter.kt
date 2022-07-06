261293c682d1fc4cbd5d2dde670a3b5aa51f3464
rehemae <rehemaephraimnanda@gmail.com>
2022-07-06 20:06:09 +0300

    Twitter Assignment

---
+++ app/src/main/java/dev/rehema/twitterassignment/Twitter.kt
@@ -0,0 +1,20 @@
+package dev.rehema.twitterassignment
+
+data class Twitter(
+var name:String,
+var username:String,
+var havators:String,
+var like:String,
+var image:String,
+var likess:String,
+var retweet:String,
+var retweets:String,
+var share:String,
+var shares:String,
+var reply:String,
+var word:String,
+var Likes:String,
+
+
+
+)
